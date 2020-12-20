2cb91e46059f41a3d73778c7228cecd9e646dcae
yusuke.nakazato <ajitama6@gmail.com>
2020-12-21 01:41:10 +0900

    テストコードを追加

---
+++ test/fizzbuzz_test.rb
@@ -0,0 +1,16 @@
+require 'minitest/autorun'
+require './fizzbazz3'
+
+class FizzBuzzTest < Minitest::Test
+  # テストメソッドを実行
+  def test_fizzbuzz
+    assert_equal '1', fizzbuzz(1)
+    assert_equal '2', fizzbuzz(2)
+    assert_equal 'Fizz', fizzbuzz(3)
+    assert_equal '4', fizzbuzz(4)
+    assert_equal 'Buzz', fizzbuzz(5)
+    assert_equal 'Fizz', fizzbuzz(6)
+    assert_equal 'FizzBuzz', fizzbuzz(15)
+  end
+end
+
